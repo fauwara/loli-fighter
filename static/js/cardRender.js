f5576aa3ca0d7a49b7a316659d2e93cf68a2c573
Fawaz Abid <53698026+fazxza@users.noreply.github.com>
2020-10-13 18:23:37 +0530

    Update cardRender.js

--- static/js/cardRender.js
+++ static/js/cardRender.js
@@ -3,34 +3,42 @@ function renderCard(list, iterator){
     //-------------------------------------- card - template -------------------------------------- 
     let cardTemplate = document.createElement('div');
     cardTemplate.setAttribute('class', 'card');
+    cardTemplate.style.border = `5px solid ${list[iterator].loliClass.color}`
 
 
     //----------------------------------------- card - img ----------------------------------------
-    let cardImage = document.createElement('img');
-    cardImage.setAttribute('class', 'card-img');
-    cardTemplate.appendChild(cardImage);
-    cardImage.setAttribute('src',list[iterator].imgLink);
+    cardTemplate.style.backgroundImage = `url('${list[iterator].imgLink}')`;
     
     //------------------------------------------ card - bio --------------------------------------
     let cardBio = document.createElement('div');
     cardBio.setAttribute('class', 'card-bio');
     cardTemplate.appendChild(cardBio);
 
+    // top part of card
+    let cardTop = document.createElement('div');
+    cardTop.setAttribute('class', 'card-Top');
+    cardBio.appendChild(cardTop);
+
     // ----------------------------------------- card - header ----------------------------------
     let cardHeader = document.createElement('div');
     cardHeader.setAttribute('class', 'card-header');
-    cardBio.appendChild(cardHeader);
+    cardTop.appendChild(cardHeader);
     
+    // name - element
+    let cardNameElement = document.createElement('div');
+    cardNameElement.setAttribute('class', 'card-name-element');
+    cardHeader.appendChild(cardNameElement);
+
     // card - name
     let cardLoliName = document.createElement('div');
     cardLoliName.setAttribute('class', 'card-header-loliname');
-    cardHeader.appendChild(cardLoliName);
+    cardNameElement.appendChild(cardLoliName);
     cardLoliName.innerText = list[iterator].loliName;
     
     // card - category
     let cardLoliCategory = document.createElement('div');
     cardLoliCategory.setAttribute('class', 'card-header-lolicategory');
-    cardHeader.appendChild(cardLoliCategory);
+    cardNameElement.appendChild(cardLoliCategory);
     cardLoliCategory.innerText = list[iterator].loliCategory;
     
     // card - class
@@ -38,37 +46,48 @@ function renderCard(list, iterator){
     cardLoliClass.setAttribute('class', 'card-header-loliclass');
     cardHeader.appendChild(cardLoliClass);
     cardLoliClass.innerText = list[iterator].loliClass.className;
+    cardLoliClass.style.color = list[iterator].loliClass.color;
+    
 
     // loli - stats
     //--------------------------------------------- card - stats ----------------------------------
     let cardStats = document.createElement('div');
     cardStats.setAttribute('class', 'card-stats');
-    cardBio.appendChild(cardStats);
-    // card - hp
-    let cardStatsHp = document.createElement('div');
-    cardStatsHp.setAttribute('class', 'card-stats-Hp');
-    cardStats.appendChild(cardStatsHp);
-    cardStatsHp.innerText = list[iterator].stats.hp;
+    cardTop.appendChild(cardStats);
     // card - attack
     let cardStatsAttack = document.createElement('div');
     cardStatsAttack.setAttribute('class', 'card-stats-attack');
     cardStats.appendChild(cardStatsAttack);
-    cardStatsAttack.innerText = list[iterator].stats.attack;
+    cardStatsAttack.innerText = `attack: ${list[iterator].stats.attack}`;
     // card - defence
     let cardStatsDefence = document.createElement('div');
     cardStatsDefence.setAttribute('class', 'card-stats-defence');
     cardStats.appendChild(cardStatsDefence);
-    cardStatsDefence.innerText = list[iterator].stats.defence;
+    cardStatsDefence.innerText = `defence: ${list[iterator].stats.defence}`;
     // card - speed
     let cardStatsSpeed = document.createElement('div');
     cardStatsSpeed.setAttribute('class', 'card-stats-speed');
     cardStats.appendChild(cardStatsSpeed);
-    cardStatsSpeed.innerText = list[iterator].stats.speed;
+    cardStatsSpeed.innerText = `speed: ${list[iterator].stats.speed}`;
+    
+    
+    
 
+    // -------------------------------------- card - bottom half ------------------------------
+    let bottomCard = document.createElement('div');
+    bottomCard.setAttribute('class', 'bottom-card');
+    cardBio.appendChild(bottomCard);
+
+    // card - hp
+    let cardStatsHp = document.createElement('div');
+    cardStatsHp.setAttribute('class', 'card-stats-hp');
+    bottomCard.appendChild(cardStatsHp);
+    cardStatsHp.innerText = `HP: ${list[iterator].stats.hp}`;
+    
     // ----------------------------------------- card - moveset --------------------------------
     let cardMoveSet = document.createElement('div');
     cardMoveSet.setAttribute('class','card-moveset');
-    cardBio.appendChild(cardMoveSet);
+    bottomCard.appendChild(cardMoveSet);
 
     // loli - moves
     for(let j = 0; j < 4; j++){
@@ -76,12 +95,10 @@ function renderCard(list, iterator){
         cardMoveElement.setAttribute('class', 'card-move');
         cardMoveElement.innerText = list[iterator].moveSet[j].moveName;
         cardMoveSet.appendChild(cardMoveElement);
+        cardMoveElement.style.border = `${list[iterator].loliClass.color} 5px solid`
+        cardMoveElement.style.backgroundColor = `${list[iterator].loliClass.color}`
     }
 
-    document.getElementsByClassName('loli-page')[0].appendChild(cardTemplate);
+    document.getElementsByClassName('loli-list')[0].appendChild(cardTemplate);
 
 }
-
-// for(let i = 0; i < loliList.length; i++){
-//     renderCard(loliList);
-// }
